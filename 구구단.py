22337d9bbb31627e46a9e38de2b3bd10a4bad9e2
dalsjjj <mmssjj0403@naver.com>
2025-04-10 15:59:40 +0900

    Add files via upload

---
+++ 구구단.py
@@ -0,0 +1,8 @@
+for dan in range(2, 10):
+    print(f"#  {dan}단  #", end=" ")
+print()
+
+for i in range(1, 10):
+    for dan in range(2, 10):
+        print(f"{dan}X{i} = {dan*i:2}", end="  ")
+    print()
